73813df69324119583a4fe6249b3699947cca6ac
Fritz Gerneth <fgerneth@effms.de>
2013-04-10 13:06:47 +0200

    Inial code commit

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+    summary: 'Reactive client-side router adapter for ia-router-core'
+});
+
+Package.on_use(function (api, where) {
+    api.use(['ia-router-core', 'deps'], 'client');
+
+    api.add_files('lib/Reactive.js', 'client');
+});
+
+Package.on_test(function (api) {
+    api.use(['ia-router-reactive', 'deps', 'tinytest']);
+
+    api.add_files('tests/ReactiveTest.js', 'client');
+});
